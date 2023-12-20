01090c70d63e80edf6abe0ef9a28d84b688a2fe8
Lefteris Kokkinelis <elkokkinelis@gmail.com>
2023-12-20 20:24:45 +0200

    Finished  Projects Page

--- src/constants/index.js
+++ src/constants/index.js
@@ -133,12 +133,12 @@ export const socialLinks = [
     {
         name: 'GitHub',
         iconUrl: github,
-        link: 'https://github.com/YourGitHubUsername',
+        link: 'https://github.com/katzarelis',
     },
     {
         name: 'LinkedIn',
         iconUrl: linkedin,
-        link: 'https://www.linkedin.com/in/YourLinkedInUsername',
+        link: 'https://www.linkedin.com/in/lefteris-kokkinelis/',
     }
 ];
 
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,4 @@
-import { useState, Suspense } from 'react'
+import { useState, Suspense, useEffect, useRef } from 'react'
 import Loader from '../components/Loader'
 import { Canvas } from '@react-three/fiber'
 
@@ -9,11 +9,27 @@ import Plane from '../models/Plane';
 import Island from '../models/island';
 import HomeInfo from '../components/HomeInfo';
 
+import sakura from '../assets/sakura.mp3'
+import { soundoff, soundon } from '../assets/icons';
+
 
 
 const Home = () => {
+  const audioRef = useRef(new Audio(sakura));
+  audioRef.current.volume = 0.4;
+  audioRef.current.loop = true;
   const [isRotating, setIsRotating] = useState(false);
   const [CurrentStage, setCurrentStage] = useState(1)
+  const [isPlayingMusic, setIsPlayingMusic] = useState(false);
+
+  useEffect(() =>{
+    if(isPlayingMusic) {
+      audioRef.current.play();
+    }
+    return () => {
+      audioRef.current.pause();
+    }
+  }, [isPlayingMusic])
 
   const adjustIslandForScreenSize = () => {
     let screenScale = null;
@@ -76,6 +92,15 @@ const Home = () => {
            />
         </Suspense>
       </Canvas>
+
+      <div className='absolute bottom-2 left-2'>
+        <img
+         src={!isPlayingMusic ? soundoff :soundon}
+         alt='sound'
+         className='w-10 h-10 cursor-pointer object-contain'
+         onClick={() => setIsPlayingMusic(!isPlayingMusic)}
+        />
+      </div>
     </section>
   )
 }
